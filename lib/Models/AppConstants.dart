b096c149e8c9735689c126eba545fc70d376e594
Shane <shanewilsonpro@gmail.com>
2020-05-18 21:20:20 -0700

    adding welcome screen

---
+++ lib/Models/AppConstants.dart
@@ -0,0 +1,3 @@
+class AppConstants {
+  static final String appName = 'Hotel Hunter';
+}
---
+++ lib/Screens/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:rental_app/Models/AppConstants.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.hotel,
+              size: 80,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Text('Welcome to ${AppConstants.appName}',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+              ),
+              textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
