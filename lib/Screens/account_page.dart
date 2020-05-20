20e0fe4eb8a010f88ad8c4bfe5e53ad0e9154756
Shane <shanewilsonpro@gmail.com>
2020-05-19 21:48:42 -0700

    adding account page

--- lib/Screens/account_page.dart
+++ lib/Screens/account_page.dart
@@ -1,7 +1,8 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:hotel_hunter_app/Screens/login_page.dart';
 
 class AccountPage extends StatefulWidget {
-
   AccountPage({Key key}) : super(key: key);
 
   @override
@@ -9,10 +10,112 @@ class AccountPage extends StatefulWidget {
 }
 
 class _AccountPageState extends State<AccountPage> {
+  void _logout() {
+    Navigator.pushNamed(context, LoginPage.routeName);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Account Page'),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 50, 20, 0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 30.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                MaterialButton(
+                  onPressed: () {},
+                  child: CircleAvatar(
+                    backgroundColor: Colors.black,
+                    radius: MediaQuery.of(context).size.width / 9.5,
+                    child: CircleAvatar(
+                      backgroundImage:
+                          AssetImage('assets/images/profile_demo.jpg'),
+                      radius: MediaQuery.of(context).size.width / 10,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 6.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      AutoSizeText(
+                        'Shane Wilson',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                        ),
+                      ),
+                      AutoSizeText(
+                        'shanewilsonpro@gmail.com',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ListView(
+            shrinkWrap: true,
+            children: <Widget>[
+              MaterialButton(
+                height: MediaQuery.of(context).size.height / 9.0,
+                onPressed: () {},
+                child: AccountPageListTile(
+                  text: 'Personal Information',
+                  iconData: Icons.person,
+                ),
+              ),
+              MaterialButton(
+                height: MediaQuery.of(context).size.height / 9.0,
+                onPressed: () {},
+                child: AccountPageListTile(
+                  text: 'Become a Host',
+                  iconData: Icons.hotel,
+                ),
+              ),
+              MaterialButton(
+                height: MediaQuery.of(context).size.height / 9.0,
+                onPressed: _logout,
+                child: AccountPageListTile(
+                  text: 'Logout',
+                  iconData: null,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AccountPageListTile extends StatelessWidget {
+  final String text;
+  final IconData iconData;
+
+  AccountPageListTile({Key key, this.text, this.iconData}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.all(0.0),
+      leading: Text(
+        this.text,
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.normal,
+        ),
+      ),
+      trailing: Icon(this.iconData),
     );
   }
 }
