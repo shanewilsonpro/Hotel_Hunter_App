051b71990c3eb657fefa5eeacb0f78484303d1d7
Shane <shanewilsonpro@gmail.com>
2020-05-21 23:06:02 -0700

    adding explore search page and posting tile widget

--- lib/Screens/explore_page.dart
+++ lib/Screens/explore_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hotel_hunter_app/Views/grid_widgets.dart';
 
 class ExplorePage extends StatefulWidget {
-
   ExplorePage({Key key}) : super(key: key);
 
   @override
@@ -11,8 +11,45 @@ class ExplorePage extends StatefulWidget {
 class _ExplorePageState extends State<ExplorePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Explore Page'),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(25, 25, 25, 0),
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                      width: 2.0,
+                    ),
+                  ),
+                  contentPadding: EdgeInsets.all(5.0),
+                ),
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            GridView.builder(
+                shrinkWrap: true,
+                itemCount: 3,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 15,
+                    mainAxisSpacing: 15,
+                    childAspectRatio: 3 / 4),
+                itemBuilder: (context, index) {
+                  return PostingGridTile();
+                }),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/Views/grid_widgets.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter_rating/flutter_rating.dart';
+import 'package:hotel_hunter_app/Models/app_constants.dart';
+
+class PostingGridTile extends StatefulWidget {
+  PostingGridTile({Key key}) : super(key: key);
+
+  @override
+  _PostingGridTileState createState() => _PostingGridTileState();
+}
+
+class _PostingGridTileState extends State<PostingGridTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        AspectRatio(
+          aspectRatio: 3 / 2,
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/apartment.jpg'),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+        ),
+        AutoSizeText(
+          'Apartment - Los Angeles, CA',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+          ),
+        ),
+        AutoSizeText(
+          'Awesome Apartment',
+          style: TextStyle(
+            fontSize: 14,
+          ),
+        ),
+        Text('\$120 / night'),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            StarRating(
+              size: 15.0,
+              starCount: 5,
+              color: AppConstants.selectedIconColor,
+              borderColor: Colors.grey,
+              onRatingChanged: null,
+              rating: 4.5,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
